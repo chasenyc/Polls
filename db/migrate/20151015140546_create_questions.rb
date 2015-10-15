1965112750d0b28885db21be03622ba936de01c1
AlexandJustin <AlexandJustin@laffcademy>
2015-10-15 10:22:24 -0400

    Add all tables with indices and blank models.

---
+++ db/migrate/20151015140546_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.text :text
+      t.integer :poll_id
+
+      t.timestamps null: false
+    end
+
+    add_index :questions, :poll_id
+  end
+end
